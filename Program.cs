2c1bddba0382d14e140f8a36e0933ea4c3946528
amir ekbatani <amir.ekbatani@gmail.com>
2021-01-25 11:51:12 +0330

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2543
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
